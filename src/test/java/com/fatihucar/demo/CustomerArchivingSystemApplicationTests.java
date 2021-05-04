7d570820375bf60220f1ff1c1cfe26709d01d4f3
Fatih Uçar <fatihucar@me.com>
2021-05-05 00:32:55 +0300

    proje başlangıcı

---
+++ src/test/java/com/fatihucar/demo/CustomerArchivingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatihucar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerArchivingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
