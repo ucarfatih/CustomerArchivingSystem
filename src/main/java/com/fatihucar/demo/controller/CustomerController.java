87505481d35c2d7ce2318eb8cd000fa9a6aa9995
Fatih Uçar <fatihucar@me.com>
2021-05-05 23:11:05 +0300

    Customer CRUD operations implemented

--- src/main/java/com/fatihucar/demo/controller/CustomerController.java
+++ src/main/java/com/fatihucar/demo/controller/CustomerController.java
@@ -1,17 +1,24 @@
 package com.fatihucar.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatihucar.demo.exception.ResourceNotFoundException;
 import com.fatihucar.demo.model.Customer;
 import com.fatihucar.demo.repository.CustomerRepository;
+import com.fatihucar.demo.util.Constants;
 
 import javassist.NotFoundException;
 
@@ -31,7 +38,34 @@ public class CustomerController {
 	public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Long customerId)
 			throws NotFoundException, ResourceNotFoundException {
 		final Customer customer = customerRepository.findById(customerId)
-				.orElseThrow(() -> new ResourceNotFoundException("not found"));
+				.orElseThrow(() -> new ResourceNotFoundException(Constants.ERR_MSG + customerId));
 		return ResponseEntity.ok().body(customer);
 	}
+
+	@PostMapping(path = "/customer")
+	public Customer createCustomer(@RequestBody Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	@PutMapping(path = "/customer/{id}")
+	public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Long customerId,
+			@RequestBody Customer updatedCustomer) throws ResourceNotFoundException {
+		final Customer customer = customerRepository.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException(Constants.ERR_MSG + customerId));
+		updatedCustomer.setId(customer.getId());
+		return ResponseEntity.ok().body(customerRepository.save(updatedCustomer));
+
+	}
+
+	@DeleteMapping(path = "/customer/{id}")
+	public Map<String, Boolean> deleteCustomer(@PathVariable(value = "id") Long customerId)
+			throws ResourceNotFoundException {
+		final Customer customer = customerRepository.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException(Constants.ERR_MSG + customerId));
+		customerRepository.delete(customer);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put(Constants.DELETED, Boolean.TRUE);
+		return response;
+	}
+
 }
---
+++ src/main/java/com/fatihucar/demo/util/Constants.java
@@ -0,0 +1,7 @@
+package com.fatihucar.demo.util;
+
+public class Constants {
+	public static final String ERR_MSG = "Customer Not Found for This ID: ";
+	public static final String DELETED = "DELETED";
+
+}
